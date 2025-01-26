72ec0c4ce03c9ddb03940849e523a319e9a63e81
Woosuk Jang <woosukj89@gmail.com>
2025-01-27 02:22:40 +0900

    first commit

---
+++ demo/next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  reactStrictMode: true,
+  basePath: '/static-poc'
+};
+
+export default nextConfig;
