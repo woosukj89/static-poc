983a5797bb29ff3418aac3fefc754d7cf7dcd4c0
Woosuk Jang <woosukj89@gmail.com>
2025-01-27 03:58:51 +0900

    ..

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
     loader: "akamai",
     path: "",
   },
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '/',
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
 };
 
