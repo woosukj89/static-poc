6a94274b8046f4d33dc4818932ebb9615f75cb87
woosukj89 <woosukj89@users.noreply.github.com>
2025-01-26 18:07:36 +0000

    Deploying to public from @ woosukj89/static-poc@f8ef40c09629d167aa6a00225f4809827dd771d3 🚀

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
-  basePath: '/static-poc'
-};
-
-export default nextConfig;
